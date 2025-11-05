33e49b3ac6f1684f8b443dd95947deafdd9a1d82
Labgremlin <fwunderle.biz@gmail.com>
2025-11-05 19:24:06 +0100

    8: added the ability for the player to shoot; added the ability for asteroids to split

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
 from circleshape import CircleShape
+from constants import *
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -14,3 +16,17 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity*dt
         return super().update(dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        angle = random.uniform(20, 50)
+        vel1 = self.velocity.rotate(angle)
+        vel2 = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = vel1 * 1.2
+        asteroid2.velocity = vel2 * 1.2
+        
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -11,11 +12,14 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
 
     ship = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     field = AsteroidField()
@@ -34,6 +38,12 @@ def main():
             if entity.collision(ship):
                 print("Game Over!")
                 exit()
+
+        for bullet in shots:
+            for rock in asteroids:
+                if bullet.collision(rock):
+                    bullet.kill()
+                    rock.split()
 
         for entity in drawable:
             entity.draw(screen)
