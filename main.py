435faa8a1d5d0f519cec57a1f3617aebb803bdf9
Labgremlin <fwunderle.biz@gmail.com>
2025-11-05 18:08:35 +0100

    4: added player ship

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pygame
 from constants import *
+import player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    ship = player.Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black",rect=None,special_flags=0)
+        ship.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
